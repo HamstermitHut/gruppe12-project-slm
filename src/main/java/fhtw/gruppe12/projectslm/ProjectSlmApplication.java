08646f0f69898289ce0dec6b584db2bdb5bb27a2
Markus Frank <ic22b072@technikum-wien.at>
2022-12-30 14:05:28 +0100

    project initialization

---
+++ src/main/java/fhtw/gruppe12/projectslm/ProjectSlmApplication.java
@@ -0,0 +1,13 @@
+package fhtw.gruppe12.projectslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/gruppe12/projectslm/ProjectSlmApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.gruppe12.projectslm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
