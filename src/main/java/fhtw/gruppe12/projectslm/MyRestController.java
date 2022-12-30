7faf425bb56f40a9d17bd0ca5225cd1312b80921
Markus Frank <ic22b072@technikum-wien.at>
2022-12-30 14:24:50 +0100

    Rest Controller

---
+++ src/main/java/fhtw/gruppe12/projectslm/MyRestController.java
@@ -0,0 +1,10 @@
+package fhtw.gruppe12.projectslm;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+    final String std_mes = "Everything works as expected";
+    String var_mes = null;
+}
